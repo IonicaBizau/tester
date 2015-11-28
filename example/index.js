df3663ea268cf1d7ac47e937f6d299d772e031fc
Ionică Bizău <bizauionica@gmail.com>
2015-11-28 17:58:10 +0200

    Initial commit

---
+++ example/index.js
@@ -0,0 +1,12 @@
+const tester = require("../");
+
+example.describe("Running some example tests", example => {
+
+    example.it("should be able to add instances", () => {
+        this.expect(true).toBe(true);
+    });
+
+    example.it("should be able to add instances", () => {
+        this.expect(true).toBe(true);
+    });
+});
