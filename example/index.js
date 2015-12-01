c73eca3deefee3875234ec4285e777a25c474e1b
Ionică Bizău <bizauionica@gmail.com>
2015-12-01 11:11:22 +0200

    :memo:

--- example/index.js
+++ example/index.js
@@ -1,12 +1,32 @@
 const tester = require("../");
 
-example.describe("Running some example tests", example => {
+// Use describe to group the tests
+tester.describe("Running some example tests", example => {
 
-    example.it("should be able to add instances", () => {
-        this.expect(true).toBe(true);
+    // Without a callback (the code will be executed syncronously)
+    example.it("should be able to make simple checks using expect", () => {
+        example.expect(true).toBe(true);
     });
 
-    example.it("should be able to add instances", () => {
-        this.expect(true).toBe(true);
+    // Async function which will fail
+    example.it("should support async functions", (cb) => {
+        setTimeout(function() {
+            example.expect(true).toBe(false);
+            cb();
+        }, 100);
+    });
+
+    // Wait a second and pass
+    example.it("wait a second", (cb) => {
+        setTimeout(function() {
+            example.expect(true).toBe(true);
+            cb();
+        }, 1000);
+    });
+});
+
+tester.describe("Running another set of tests", another => {
+    another.it("some test", () => {
+        another.expect(true).toBe(true);
     });
 });
