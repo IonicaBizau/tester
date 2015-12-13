190d4ad9036aa4fe75ca31efc37dffc1bb31a8b3
Ionică Bizău <bizauionica@gmail.com>
2015-12-13 11:16:28 +0200

    Added the should method

--- example/index.js
+++ example/index.js
@@ -4,12 +4,12 @@ const tester = require("../");
 tester.describe("Running some example tests", example => {
 
     // Without a callback (the code will be executed syncronously)
-    example.it("should be able to make simple checks using expect", () => {
+    example.should("be able to make simple checks using expect", () => {
         example.expect(true).toBe(true);
     });
 
     // Async function which will fail
-    example.it("should support async functions", (cb) => {
+    example.should("support async functions", (cb) => {
         setTimeout(function() {
             example.expect(true).toBe(false);
             cb();
