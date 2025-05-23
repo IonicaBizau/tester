ad484ca696f08d275214d8c47d671614d57160f4
Ionică Bizău <bizauionica@gmail.com>
2025-05-23 12:57:15 +0000

    Bump the version of expect and fix errors

--- test/index.js
+++ test/index.js
@@ -3,7 +3,7 @@ const tester = require("../");
 // Use describe to group the tests
 tester.describe("tester", test => {
     test.it("describe should be a function", () => {
-        test.expect(tester.describe).toBeA(Function);
+        test.expect(typeof tester.describe).toBe('function');
     });
 });
 
